ebabf3a1ecf8409580bc79f93f50257eab8e369e
Juan Marco <jmmarco@gmail.com>
2022-03-28 10:37:17 +0200

    Build keypad dynamically

--- index.mjs
+++ index.mjs
@@ -1,17 +1,29 @@
+import { isElement } from "./utils.mjs";
+
 function operate(operator, a, b) {
-  switch(operator) {
-    case '+':
+  switch (operator) {
+    case "+":
       return a + b;
-    case '-':
+    case "-":
       return a - b;
-    case '*':
+    case "*":
       return a * b;
-    case '/':
+    case "/":
       return a / b;
     default:
       return;
   }
 }
 
+function populateKeyPad(targetElement) {
+  if (!isElement(targetElement)) return;
 
-console.log(operate('+', 5, 7));
+  new Array(10).fill("").forEach((digit, index) => {
+    const key = document.createElement("div");
+    key.textContent = index;
+    key.dataset.keyValue = index;
+    targetElement.appendChild(key);
+  });
+}
+
+populateKeyPad(document.querySelector(".calculator-keypad"));
