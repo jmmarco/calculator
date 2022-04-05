ca6d9f7d8cfadd3ad552a19855ef9840d5c2557c
Juan Marco <jmmarco@gmail.com>
2022-04-05 15:46:12 +0200

    Add necessary helper functions

--- utils.mjs
+++ utils.mjs
@@ -1,3 +1,34 @@
 export function isElement(element) {
   return element instanceof Element || element instanceof Document;
 }
+
+export function isNumber(n) {
+  return !isNaN(n);
+}
+
+export function clearState(obj, key) {
+  if (!key) {
+    for (let k in obj) {
+      obj[k] = "";
+    }
+  }
+
+  if (key in obj) {
+    obj[key] = "";
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+export function mapToOperator(operator) {
+  switch (operator) {
+    case "÷":
+      return "/";
+    case "x":
+      return "*";
+    default:
+      return operator;
+  }
+}
